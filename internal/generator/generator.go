70b29b5b451dd4f4b8b9468cf571f3ac441194d9
md.zonieedhossain <zonieed.uap@gmail.com>
2025-03-30 15:39:46 +0600

    feat: generate main.go with starter code for fiber, echo, or gin

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,6 +1,7 @@
 package generator
 
 import (
+	"fmt"
 	"html/template"
 	"os"
 	"path/filepath"
@@ -33,12 +34,26 @@ func Generate(opts Options) error {
 		}
 	}
 
-	// Load main.go template
+	var mainTemplate string
+	switch opts.Web {
+	case "fiber":
+		mainTemplate = "internal/templates/main_fiber.go.tmpl"
+	case "echo":
+		mainTemplate = "internal/templates/main_echo.go.tmpl"
+	case "gin":
+		mainTemplate = "internal/templates/main_gin.go.tmpl"
+	default:
+		return fmt.Errorf("unsupported web framework: %s", opts.Web)
+	}
+
+	// Load template
 	_ = renderTemplate("internal/templates/main.go.tmpl", filepath.Join(base, "main.go"), opts)
 	_ = renderTemplate("internal/templates/env.tmpl", filepath.Join(base, ".env"), opts)
 	_ = renderTemplate("internal/templates/Dockerfile.tmpl", filepath.Join(base, "Dockerfile"), opts)
 	_ = renderTemplate("internal/templates/go.mod.tmpl", filepath.Join(base, "go.mod"), opts)
 	_ = renderTemplate("internal/templates/README.md.tmpl", filepath.Join(base, "README.md"), opts)
+	_ = renderTemplate("internal/templates/Makefile.tmpl", filepath.Join(base, "Makefile"), opts)
+	_ = renderTemplate(mainTemplate, filepath.Join(base, "cmd", "main.go"), opts)
 
 	return nil
 }
